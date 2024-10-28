2cb03659962b23e2bfe2f51309fab110a19d2883
hikaru-yamaguchi-neuro <neurosatomlym@gmail.com>
2024-10-28 17:06:29 +0900

    Add dataset.py

---
+++ utils/dataset.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from pathlib import Path
+from tifffile import TiffFile
+import gc
+
+
+def make_dataset(filename):
+    with TiffFile(filename) as tif:
+        tmp_numpy = tif.asarray()
+
+    tmp_tensor = tf.convert_to_tensor(tmp_numpy, dtype=tf.float32)
+
+    dataset = tf.data.Dataset.from_tensor_slices(tmp_tensor)
+
+    del tmp_numpy
+    del tmp_tensor
+    gc.collect()
+
+    return dataset
